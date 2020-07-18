84aff1d3d8f3d40e5b1c16d97abfd218ca7167f7
Khanh <tran_khanh@outlook.com>
2020-07-18 12:50:27 +0700

    update

--- src/Library/components/Navigation/index.js
+++ src/Library/components/Navigation/index.js
@@ -8,31 +8,42 @@ import NowPlayingNav from '../NowPlayingNav'
 
 import { container, navItemsContainer } from './NavigationStyles.css'
 
-const Navigation = ({ handlePause, handleResume, handleRestart, handleNext, isPlaying, currentTrack, currentArtwork, currentTime, duration }) => (
-  <div className={container}>
-    <ul className={navItemsContainer}>
-      <Link to={`/songs`}>
-        Songs
+const Navigation = () => {
+  const [handlePause,
+    handleResume,
+    handleRestart,
+    handleNext,
+    isPlaying,
+    currentTrack,
+    currentArtwork,
+    currentTime,
+    duration] = arguments
+  return (
+    <div className={container}>
+      <ul className={navItemsContainer}>
+        <Link to={`/songs`}>
+          Songs
       </Link>
-      <Link to={`/artists`}>
-        Artists
+        <Link to={`/artists`}>
+          Artists
       </Link>
-      <Link to={`/playlists`}>
-        Playlists
+        <Link to={`/playlists`}>
+          Playlists
       </Link>
-    </ul>
-    <NowPlayingNav 
-      handlePause={handlePause} 
-      handleResume={handleResume} 
-      handleRestart={handleRestart}
-      handleNext={handleNext} 
-      isPlaying={isPlaying}
-      currentTrack={currentTrack}
-      currentArtwork={currentArtwork}
-      currentTime={currentTime}
-      duration={duration}
-    />
-  </div>
-)
+      </ul>
+      <NowPlayingNav
+        handlePause={handlePause}
+        handleResume={handleResume}
+        handleRestart={handleRestart}
+        handleNext={handleNext}
+        isPlaying={isPlaying}
+        currentTrack={currentTrack}
+        currentArtwork={currentArtwork}
+        currentTime={currentTime}
+        duration={duration}
+      />
+    </div>
+  )
+}
 
 export default Navigation;
--- src/Library/components/NowPlayingNav/index.js
+++ src/Library/components/NowPlayingNav/index.js
@@ -7,42 +7,63 @@ import {
 
 import Progress from '../Progress'
 
-import { container, topSectionContainer, npImg, npDetailsContainer, npTitle, npArtist, npAlbum, npControllerContainer,
-controlButtons, controlButton, npImgContainer, npImgAlt } from './NowPlayingNavStyles.css'
+import {
+  container, topSectionContainer, npImg, npDetailsContainer, npTitle, npArtist, npAlbum, npControllerContainer,
+  controlButtons, controlButton, npImgContainer, npImgAlt
+} from './NowPlayingNavStyles.css'
 
-const NowPlayingNav = ({ handlePause, handleResume, handleRestart, handleNext, isPlaying, currentTrack, currentArtwork, currentTime, duration }) => {
+/**
+ * Overloading 
+ */
+const NowPlayingNav = () => {
+  /**
+   * PROPS
+   */
+  const [
+    handlePause,
+    handleResume,
+    handleRestart,
+    handleNext,
+    isPlaying,
+    currentTrack,
+    currentArtwork,
+    currentTime,
+    duration] = arguments;
+  /**
+  * VIEW
+  */
   return (
     <div className={container}>
       <div className={topSectionContainer}>
 
-      <div className={npImgContainer}>
-        {
-          currentArtwork
-            ? <img className={npImg} src={currentArtwork}/>
-            : <div className={npImgAlt}></div>
-        }
-      </div>
+        <div className={npImgContainer}>
+          {
+            currentArtwork
+              ? <img className={npImg} src={currentArtwork} />
+              : <div className={npImgAlt}></div>
+          }
+        </div>
 
         <div className={npDetailsContainer}>
           <div className={npTitle}>{currentTrack ? currentTrack.title : "Nothing playing yet"}</div>
-          <div className={npArtist}>{currentTrack ? currentTrack.artist : '' }</div>
-          <div className={npAlbum}>{currentTrack ? currentTrack.album : '' }</div>
+          <div className={npArtist}>{currentTrack ? currentTrack.artist : ''}</div>
+          <div className={npAlbum}>{currentTrack ? currentTrack.album : ''}</div>
         </div>
         <div className={npControllerContainer}>
           <div className={controlButtons}>
-            <img onClick={handleRestart} className={controlButton} src={require('./assets/previous-track.svg')}/>
+            <img onClick={handleRestart} className={controlButton} src={require('./assets/previous-track.svg')} />
 
             {
               isPlaying
-                ? <img onClick={handlePause} className={controlButton} src={require('./assets/pause-button.svg')}/>
-                : <img onClick={handleResume} className={controlButton} src={require('./assets/play-arrow.svg')}/>
+                ? <img onClick={handlePause} className={controlButton} src={require('./assets/pause-button.svg')} />
+                : <img onClick={handleResume} className={controlButton} src={require('./assets/play-arrow.svg')} />
             }
-            
-            <img onClick={handleNext} className={controlButton} src={require('./assets/play-next-button.svg')}/>
+
+            <img onClick={handleNext} className={controlButton} src={require('./assets/play-next-button.svg')} />
           </div>
         </div>
       </div>
-      <Progress percent={currentTime == -1 ? 0 : currentTime / duration}/>
+      <Progress percent={currentTime == -1 ? 0 : currentTime / duration} />
     </div>
   )
 }
